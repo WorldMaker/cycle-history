9e58165c6967b74344fbcc7d8e99e22133b7d96f
Max Battcher <me@worldmaker.net>
2015-09-28 23:28:10 -0400

    :wrench: tsc build

--- history.js
+++ history.js
@@ -6,10 +6,11 @@ function historyDriver(request$) {
         .share();
     // Kick off our side effect to the browser	
     outgoing$.subscribe(function (info) { return window.history.pushState(info.state, info.title, info.url); });
-    var popState$ = rx.Observable.fromEventPattern(function (h) { return window.addEventListener('onpopstate', h); }, function (h) { return window.removeEventListener('onpopstate', h); })
+    var popState$ = rx.Observable.fromEventPattern(function (h) { return window.addEventListener('popstate', h); }, function (h) { return window.removeEventListener('popstate', h); })
         .map(function (ev) { return ev || { state: null }; }); // basic data cleanliness
     return rx.Observable.merge(popState$, outgoing$)
         .map(function (item) { return item.state; })
         .startWith(window.history.state);
 }
 exports.historyDriver = historyDriver;
+//# sourceMappingURL=history.js.map
