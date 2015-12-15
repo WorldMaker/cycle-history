9564c67ab535e877f927dff362dadf02c4eaba5b
Max Battcher <me@worldmaker.net>
2015-12-14 20:28:12 -0500

    Merge request and popstate observables sooner
    Merging the observables sooner should mean that popped states clear the distinctUntilChanged, fixing the issue where a user clicked back and then couldn't re-click the last chosen link.

--- history.js
+++ history.js
@@ -1,16 +1,17 @@
-var rx = require('rx');
-function historyDriver(request$) {
-    var outgoing$ = request$
+import * as rx from 'rx';
+export function historyDriver(request$) {
+    let popState$ = rx.Observable.fromEventPattern((h) => window.addEventListener('popstate', h), (h) => window.removeEventListener('popstate', h))
+        .map(ev => Object.assign({ pop: true }, ev || { state: null })); // basic data cleanliness
+    let history$ = request$
+        .map(info => info || { state: null }) // basic data cleanliness
+        .merge(popState$)
         .distinctUntilChanged() // avoid spamming the browser/infinite cycling
-        .map(function (info) { return info || { state: null }; }) // basic data cleanliness
         .share();
+    let outgoing$ = history$.filter(info => !info.pop);
     // Kick off our side effect to the browser	
-    outgoing$.subscribe(function (info) { return window.history.pushState(info.state, info.title, info.url); });
-    var popState$ = rx.Observable.fromEventPattern(function (h) { return window.addEventListener('popstate', h); }, function (h) { return window.removeEventListener('popstate', h); })
-        .map(function (ev) { return ev || { state: null }; }); // basic data cleanliness
-    return rx.Observable.merge(popState$, outgoing$)
-        .map(function (item) { return item.state; })
+    outgoing$.subscribe(info => window.history.pushState(info.state, info.title, info.url));
+    return history$
+        .map(item => item.state)
         .startWith(window.history.state);
 }
-exports.historyDriver = historyDriver;
 //# sourceMappingURL=history.js.map
--- history.ts
+++ history.ts
@@ -4,27 +4,29 @@ export interface HistoryInfo {
 	state: any
 	title?: string
 	url?: string
+	pop?: boolean
 }
 
 export type HistoryRequest = rx.Observable<HistoryInfo>
 export type HistoryResponse = rx.Observable<any>
 
-type StateObservable = rx.Observable<{ state: any }>
-
 export function historyDriver(request$: HistoryRequest): HistoryResponse {
-	let outgoing$ = request$
-		.distinctUntilChanged() // avoid spamming the browser/infinite cycling
+	let popState$ = rx.Observable.fromEventPattern<PopStateEvent>((h: any) => window.addEventListener('popstate', h),
+		(h: any) => window.removeEventListener('popstate', h))
+		.map(ev => Object.assign({ pop: true }, ev || { state: null })) // basic data cleanliness
+		
+	let history$ = request$
 		.map(info => info || { state: null }) // basic data cleanliness
+		.merge(popState$)
+		.distinctUntilChanged() // avoid spamming the browser/infinite cycling
 		.share()
+	
+	let outgoing$ = history$.filter(info => !info.pop)
 	
 	// Kick off our side effect to the browser	
 	outgoing$.subscribe(info => window.history.pushState(info.state, info.title, info.url))
 	
-	let popState$ = rx.Observable.fromEventPattern<PopStateEvent>((h: any) => window.addEventListener('popstate', h),
-		(h: any) => window.removeEventListener('popstate', h))
-		.map(ev => ev || { state: null }) // basic data cleanliness
-	
-	return rx.Observable.merge(<StateObservable>popState$, <StateObservable>outgoing$)
+	return history$
 		.map(item => item.state)
 		.startWith(window.history.state)
 }
